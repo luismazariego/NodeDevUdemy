f81109069321d2091fe2610e58061ce9fe0d463a
Luis Mazariego <luis@thresholdcommunications.com>
2020-12-11 11:10:47 -0600

    Add new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,8 @@
 const request = require('postman-request')
 
-const forecast = (latitude, longitude, callback) => {
+
+
+const current = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=d19cdb472244596bbeea13184d50a79c&query=' + latitude + ',' + longitude + '&units=m'
 
     request({ url, json: true }, (error, { body }) => {
@@ -9,9 +11,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. Feels like " + body.current.feelslike + ", Pressure " + body.current.pressure);
         }
     })
 }
 
-module.exports = forecast
+module.exports = current
